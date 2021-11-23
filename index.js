67e79ed012b30706fa0fcf080ae59147625b66d0
eduardamelo018 <melomariaeduarda685@gmail.com>
2021-11-23 01:36:29 +0000

    maria.api

---
+++ index.js
@@ -0,0 +1,10 @@
+
+document.querySelector("#btnConsultar").onclick = function ()
+     alert("funciona!!!")  
+     let cep = docmunt.querySelector("#cep").value
+     document.querySelector("#cidade").innerHTML = cep
+
+    
+/*const $btnConsultar = queryselector("btnConsultar")
+ocnset $cep = document.querselector("#cep")
+const $cidade = documen.queryselector("#cidade") 
